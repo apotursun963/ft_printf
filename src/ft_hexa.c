5cebbedf19121060b2dc731ef22204561440dcfc
abdullahTRS <124070221+apotursun963@users.noreply.github.com>
2024-11-03 16:03:40 +0300

    Update ft_hexa.c

--- src/ft_hexa.c
+++ src/ft_hexa.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../ft_printf.h"
 
 int	ft_write_hexa(unsigned int num, char type)
 {
